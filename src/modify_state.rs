aea49c93081df97fad8d0fd3dc58d484decde5ad
progre <progre+pgp@prgrssv.net>
2023-01-10 20:54:34 +0900

    refactor: refactor

--- src/modify_state.rs
+++ src/modify_state.rs
@@ -2,30 +2,32 @@ use windows::Win32::Devices::HumanInterfaceDevice::DIJOYSTATE;
 
 pub fn modify_state(joy_state: &mut DIJOYSTATE) {
     for pov in joy_state.rgdwPOV {
-        if (pov & 0xffffu32) != 0xffffu32 {
-            if pov < 36000u32 / 16 {
-                joy_state.lY = -1000; // 8
-            } else if pov < 36000u32 / 16 * 3 {
-                joy_state.lX = 1000; // 9
-                joy_state.lY = -1000;
-            } else if pov < 36000u32 / 16 * 5 {
-                joy_state.lX = 1000; // 6
-            } else if pov < 36000u32 / 16 * 7 {
-                joy_state.lX = 1000; // 3
-                joy_state.lY = 1000;
-            } else if pov < 36000u32 / 16 * 9 {
-                joy_state.lY = 1000; // 2
-            } else if pov < 36000u32 / 16 * 11 {
-                joy_state.lX = -1000; // 1
-                joy_state.lY = 1000;
-            } else if pov < 36000u32 / 16 * 13 {
-                joy_state.lX = -1000; // 4
-            } else if pov < 36000u32 / 16 * 15 {
-                joy_state.lX = -1000; // 7
-                joy_state.lY = -1000;
-            } else {
-                joy_state.lY = -1000;
-            }
+        if (pov & 0x0000ffff) == 0x0000ffff {
+            // neutral
+            continue;
+        }
+        if pov < 36000u32 / 16 {
+            joy_state.lY = -1000; // 8
+        } else if pov < 36000u32 / 16 * 3 {
+            joy_state.lX = 1000; // 9
+            joy_state.lY = -1000;
+        } else if pov < 36000u32 / 16 * 5 {
+            joy_state.lX = 1000; // 6
+        } else if pov < 36000u32 / 16 * 7 {
+            joy_state.lX = 1000; // 3
+            joy_state.lY = 1000;
+        } else if pov < 36000u32 / 16 * 9 {
+            joy_state.lY = 1000; // 2
+        } else if pov < 36000u32 / 16 * 11 {
+            joy_state.lX = -1000; // 1
+            joy_state.lY = 1000;
+        } else if pov < 36000u32 / 16 * 13 {
+            joy_state.lX = -1000; // 4
+        } else if pov < 36000u32 / 16 * 15 {
+            joy_state.lX = -1000; // 7
+            joy_state.lY = -1000;
+        } else {
+            joy_state.lY = -1000;
         }
     }
 }
